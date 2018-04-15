cefc8d8158f8baa62ca933c5e93c73340a6ce4cb
unknown <bubbastinx@gmail.com>
2018-04-15 08:00:23 -0600

    capturing user arguments

--- liri.js
+++ liri.js
@@ -3,8 +3,11 @@
 // var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
 
+
+// Requires the node file system to 
 let fs = require("fs");
 
+// FileSystem reading the randomtxt file, with the UTF8 encolding, with a callback function of error and data
 fs.readFile("random.txt", "utf8", function(error,data){
     if (error){
         return console.log(error);
@@ -13,4 +16,25 @@ fs.readFile("random.txt", "utf8", function(error,data){
     dataArray = data.split(",");
     console.log(dataArray);
 })
+
+// Capturing the user multiple arguments
+let nodeArgs = process.argv;
+console.log(nodeArgs)
+
+let userSearch = "";
+
+for (let i = 2; i < nodeArgs.length; i++){
+     if ( i > 2 && i < nodeArgs.length){
+         userSearch = userSearch + "+" + nodeArgs[i];
+        // console.log(UserSearch);
+     }
+     else {
+
+      userSearch += nodeArgs[i];
+     }
+}
+console.log(UserSearch);
+
+
+
 
